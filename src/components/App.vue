72c21a7f7aa3a300b2a9776ee1b4b105c5150f9a
guojinrong <guojinrong@yy.com>
2018-05-18 21:48:39 +0800

    test

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <h1>JT</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {};
+  },
+  created() {},
+}
+</script>
+
+<style lang="scss">
+  .app {
+    h1 {
+      color: green;
+    }
+  }
+</style>
+
--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-import { sum } from './common/util';
-import "../css/index.css";
-
-console.log("hello world");
-console.log(sum(101, 2321111));
