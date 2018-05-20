96befb4715cb90f95208667d02e959d244452e85
JR93 <gjr_china@163.com>
2018-05-20 23:32:39 +0800

    build

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,32 +2,41 @@
   <div class="app">
     <h1>JR666</h1>
     <div class="bd">
-      <p class="desc">Front-End {{name}}</p>
+      <input type="text" v-model="name" placeholder="input something">
+      <p class="desc" @click="output">Front-End {{name}}</p>
     </div>
   </div>
 </template>
 
 <script>
+import util from '../js/common/util'
+
 export default {
   name: 'app',
-  data() {
+  data () {
     return {
-      name: 'YY'
-    };
+      name: ''
+    }
   },
-  created() {},
+  methods: {
+    output () {
+      console.log(util.sum(3, 4))
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-  .app {
-    h1 {
-      color: green;
-    }
-    .desc {
-      color: #f60;
-      font-size: 30px;
-    }
+.app {
+  h1 {
+    color: green;
+    font-family: 'dincond';
+    font-size: 40px;
   }
+  .desc {
+    color: #f60;
+    font-size: 30px;
+  }
+}
 </style>
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import './scss/index.scss';
 import App from './components/App';
 
 new Vue({
